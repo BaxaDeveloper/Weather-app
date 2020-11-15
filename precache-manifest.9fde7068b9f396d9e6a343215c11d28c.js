422e8e5c99b8b08022c9d43c96be608d44805e90
Baxa Developer <baxa.developer@gmail.com>
2020-11-15 22:11:34 +0500

    Updates

---
+++ precache-manifest.9fde7068b9f396d9e6a343215c11d28c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad464ec4b2acd0d66b413aca665d6d3",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "d8322ae09e400a8ad466",
+    "url": "/weather-app/static/css/main.2feffb57.chunk.css"
+  },
+  {
+    "revision": "38eef57acdd878294988",
+    "url": "/weather-app/static/js/2.5fdaa7ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.5fdaa7ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8322ae09e400a8ad466",
+    "url": "/weather-app/static/js/main.c620163a.chunk.js"
+  },
+  {
+    "revision": "7c8b1809a3edae9522d9",
+    "url": "/weather-app/static/js/runtime-main.eb436962.js"
+  }
+]);
